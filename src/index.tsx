2739cb1736ac1442ecc4cbca1fc6da34f31983e2
Mufti Muflihun <meltalite@gmail.com>
2023-03-18 16:32:04 +0700

    disable mouse event

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom/client';
-import './index.css'
+import './index.scss'
 import Root from './root';
 
 const rootElement = document.getElementById('root');
